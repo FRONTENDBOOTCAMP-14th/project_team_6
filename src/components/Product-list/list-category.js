4490f87527799d0df0a30f61a6be3e5c1f0141e6
Donggle-IV <dsyun9304@gmail.com>
2025-05-28 22:00:30 +0900

    [feat] 상품리스트 카테고리 필터 리스트 추가
    스타일링/마크업 완료
    
    기능 구현 완료
    
    기타 : common css 추가
    
    close #6 7 26

---
+++ src/components/Product-list/list-category.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('[data-accordion]');
+  let activeAccordion = null;
+
+  const resetButton = document.querySelector('.button__filter--init');
+  if (resetButton) {
+    resetButton.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const checkboxes = document.querySelectorAll('.card__category-list .checkbox-input');
+
+      checkboxes.forEach((checkbox) => {
+        const label = checkbox.closest('.checkbox-label');
+        if (label) {
+          const checkIcon = label.querySelector('.icon-check');
+          const checkedIcon = label.querySelector('.icon-checked');
+
+          checkbox.checked = false;
+
+          if (checkIcon) checkIcon.style.display = 'inline-block';
+          if (checkedIcon) checkedIcon.style.display = 'none';
+        }
+      });
+
+      void document.body.offsetHeight;
+    });
+
+    document.addEventListener('change', function (e) {
+      if (e.target.classList.contains('checkbox-input')) {
+        const checkbox = e.target;
+        const label = checkbox.closest('.checkbox-label');
+        if (label) {
+          const checkIcon = label.querySelector('.icon-check');
+          const checkedIcon = label.querySelector('.icon-checked');
+
+          if (checkIcon) checkIcon.style.display = checkbox.checked ? 'none' : 'inline-block';
+          if (checkedIcon) checkedIcon.style.display = checkbox.checked ? 'inline-block' : 'none';
+        }
+      }
+    });
+  }
+
+  accordions.forEach((accordion) => {
+    const header = accordion.querySelector('.c-accordion__header');
+
+    header.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (accordion === activeAccordion) {
+        accordion.classList.remove('active');
+        activeAccordion = null;
+        accordions.forEach((acc) => (acc.style.display = 'block'));
+      } else {
+        if (activeAccordion) {
+          activeAccordion.classList.remove('active');
+        }
+        accordion.classList.add('active');
+        activeAccordion = accordion;
+        accordions.forEach((acc) => {
+          acc.style.display = acc === accordion ? 'block' : 'none';
+        });
+      }
+    });
+  });
+});
